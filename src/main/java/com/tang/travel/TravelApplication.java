f2bd781463c3a0536ce60031e17aa6bc74ab79f3
唐云 <tangyun1992@qq.com>
2023-06-09 09:50:24 +0800

    集成热部署 增加hello测试接口

--- src/main/java/com/tang/travel/TravelApplication.java
+++ src/main/java/com/tang/travel/TravelApplication.java
@@ -1,13 +1,23 @@
 package com.tang.travel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class TravelApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TravelApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(TravelApplication.class, args);
+
+        SpringApplication app = new SpringApplication(TravelApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
